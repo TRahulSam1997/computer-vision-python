eb89bc1197c4b450b6c583c0a810fc3d2e9bb454
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-01-01 21:22:30 +1100

    hand trackong module modelComplexity updates

--- FaceMeshProject/FaceMeshModule.py
+++ FaceMeshProject/FaceMeshModule.py
@@ -4,16 +4,17 @@ import time
 
 class FaceMeshDetector():
 
-    def __init__(self, staticMode=False, maxFaces=2, minDetectionCon=0.5, minTrackCon=0.5):
+    def __init__(self, staticMode=False, maxFaces=2, modelComplexity=1, minDetectionCon=0.5, minTrackCon=0.5):
         self.staticMode = staticMode
         self.maxFaces = maxFaces
+        self.modelComplex = modelComplexity
         self.minDetectionCon = minDetectionCon
         self.minTrackCon = minTrackCon
 
         self.mpDraw = mp.solutions.drawing_utils
         self.mpFaceMesh = mp.solutions.face_mesh
         self.faceMesh = self.mpFaceMesh.FaceMesh(self.staticMode, self.maxFaces,
-                                                 self.minDetectionCon, self.minTrackCon)
+                                                 self.modelComplex, self.minDetectionCon, self.minTrackCon)
         self.drawSpec = self.mpDraw.DrawingSpec(thickness=1, circle_radius=2)
 
     def findFaceMesh(self, img, draw=True):
