7047053b0222ab47a133056db89d13aa40767c5c
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-01-12 23:46:09 +1100

    pycaw added

--- HandTrackingProject/VolumeHandControl.py
+++ HandTrackingProject/VolumeHandControl.py
@@ -3,6 +3,9 @@ import time
 import numpy as np
 import HandTrackingModule as htm
 import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 ##########################
 wCam, hCam = 640, 480
@@ -15,17 +18,17 @@ pTime = 0
 
 detector = htm.handDetector(detectionCon=0.7)
 
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
-volume.GetMute()
-volume.GetMasterVolumeLevel()
-volume.GetVolumeRange()
-volume.SetMasterVolumeLevel(-20.0, None)
+
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volRange = volume.GetVolumeRange()
+volume.SetMasterVolumeLevel(0, None)
+minVol = volRange[0]
+maxVol = volRange[1]
 
 while True:
     success, img = cap.read()
